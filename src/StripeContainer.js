d33bb1904af6043b716e3a5bac74e1cddf3d9a23
mjabdi <m.jafarabdi@gmail.com>
2021-05-07 20:10:26 +0430

    first release

--- src/StripeContainer.js
+++ src/StripeContainer.js
@@ -4,7 +4,7 @@ import { Elements } from "@stripe/react-stripe-js";
 
 import { StripeForm } from "./StripeForm";
 
-const SANDBOX = true
+const SANDBOX = false
 
 const PUBLIC_KEY_TEST = "pk_test_51InSXCJ3U7h4NHwd3Nh0B0V9Z2kFcipnZDZ5OvP0Fuhm4zlUAe6mMh2yEnSytV64kkky6ahq663pmO48YtgboJjx000IK0vjNj";
 const PUBLIC_KEY_LIVE = "pk_live_51InSXCJ3U7h4NHwdQSvc3ITNikQJRw0otzlcVnUXDKrS51Xu2drNoOIfOk3VRZyJEsVarVkGJOrLz2HywPdM8Qdl006sUXBTwX";
